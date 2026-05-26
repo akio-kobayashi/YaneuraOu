8d2a7092b51c948b9a8eacac997afd737dd49029
Akio Kobayashi <kobayashi_akio@nifty.com>
2026-05-26 11:59:10 +0900

    minor revision

--- yaneuraou_python/tools/selfplay_wsl2_sample.sh
+++ yaneuraou_python/tools/selfplay_wsl2_sample.sh
@@ -13,33 +13,34 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #   /mnt/d/shogi_home/
 #     ├── eval/
 #     └── book/
-HOME_DIR="/mnt/d/shogi_home"
+HOME_DIR="/mnt/c/Users/akiok/AobaNNUE"
 
 # 2) Engine and eval settings.
 # Use absolute WSL paths for engine binaries.
 # (Optional conversion from Windows path:
 #   ENGINE_EXE="$(wslpath -u 'C:\path\to\YaneuraOu.exe')"
 # )
-ENGINE_EXE="/mnt/c/Users/yourname/engines/YaneuraOu-by-gcc.exe"
+ENGINE_EXE="/mnt/c/Users/akiok/AobaNNUE/AobaNNUE_ZEN2.exe"
 EVAL_DIR=""  # empty => use ${ENGINE_EXE%/*}/eval
-BOOK_FILE="/Users/yourname/GitHub/engines/book/start_sfens_ply32.txt"
+#BOOK_FILE="/mnt/c/Users/akiok/AobaNNUE/book/start_sfens_ply32.txt"
 
 # 3) Match conditions.
-PARALLEL_GAMES=2
-ENGINE_THREADS=1
-TOTAL_GAMES=200
+PARALLEL_GAMES=1
+ENGINE_THREADS=16
+TOTAL_GAMES=100000
 
 # Time control examples:
 #   "d12"                : fixed depth 12
 #   "b1000"              : byoyomi 1000ms
 #   "t300000/i3000"      : 5min + 3sec increment
 #   "b1000.b2000"        : different settings for engine1/engine2
-TIME_CONTROL="d12"
+TIME_CONTROL="n200000"
 
 # Opening book:
 #   BOOK_FILE=""  : start from the initial position
 #   BOOK_FILE=... : use the specified SFEN file
 #   BOOK_MOVES>0  : truncate only startpos "moves ..." prefixes
+BOOK_FILE=""
 BOOK_MOVES=24
 RAND_BOOK="--rand_book"
 
@@ -50,6 +51,7 @@ KIFU_FORMAT="csa"
 LOG_FLAG="--log"
 PARAM_LOG_PATH="" # e.g. "logs/params"
 
+# --multipv 2 --save_candidates --alt_move_prob 0.05 --alt_move_margin_cp 10
 ARGS=(
   --home "${HOME_DIR}"
   --engine1 "${ENGINE_EXE}"
@@ -59,8 +61,29 @@ ARGS=(
   --loop "${TOTAL_GAMES}"
   --time "${TIME_CONTROL}"
   --book_moves "${BOOK_MOVES}"
-  --kifu_format "${KIFU_FORMAT}"
+#  --kifu_format "${KIFU_FORMAT}"
 )
+
+MULTIPV=2
+PROB=0.05
+MARGIN=100
+TEMP=1.0
+
+if [[ -n "${TEMP}" ]]; then
+    ARGS+=(--alt_move_temperature "${TEMP}")
+fi
+if [[ -n "${MULTIPV}" ]]; then
+    ARGS+=(--multipv "${MULTIPV}")
+    ARGS+=(--save_candidates)
+fi
+
+if [[ -n "${PROB}" ]]; then
+    ARGS+=(--alt_move_prob "${PROB}")
+fi
+
+if [[ -n "${MARGIN}" ]]; then
+    ARGS+=(--alt_move_margin_cp "${MARGIN}")
+fi
 
 if [[ -n "${EVAL_DIR}" ]]; then
   ARGS+=(--eval1 "${EVAL_DIR}" --eval2 "${EVAL_DIR}")
